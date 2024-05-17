f7cfa7aeace7251160c6f9594cda2ac8440c08e7
Ondess1 <164676566+Ondess1@users.noreply.github.com>
2024-05-17 10:21:23 +0200

    Update email.sh

--- email.sh
+++ email.sh
@@ -2,10 +2,10 @@
 
 # Set the subject and body of the email
 SUBJECT="New Blog Post Published!"
-BODY="A new post has been published on my blog. Check it out at: [https://github.com/23W-GBAC/enaJohn/tree/main]"
+BODY="A new post has been published on my blog. Check it out at: [https://github.com/Ondess1/Ondess_GBE/tree/main]"
 
 # Loop through the list of subscribers and send email to each one
 while IFS= read -r email; do
-		    # Send the email using the 'mail' command
-		    	        echo "$BODY" | mail -s "$SUBJECT" "$email"
-					done < "subscribers.txt"
+	    # Send the email using the 'mail' command
+	        echo "$BODY" | mail -s "$SUBJECT" "$email"
+	done < "subscribers.txt"
